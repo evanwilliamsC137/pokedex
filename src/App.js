2ad800018d9576bf8e34d6c935eb4e15f08b54be
Evan Williams <evan.b.williams95@gmail.com>
2021-09-29 12:08:00 -0500

    displayed pokemon in cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 
 import './App.css';
+import Card from './components/Card'
 import React, {useState, useEffect} from "react";
+import {getAllPokemon, getPokemon} from "./services/pokemon";
 
 
 function App() {
   const [pokemonData, setPokemonData] = useState(null);
   const initialUrl = `https://pokeapi.co/api/v2/pokemon?limit=151`
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
-    fetch(initialUrl)
-        .then(res => res.json())
-        .then(setPokemonData)
-  },[])
+    async function fetchData() {
+      let response = await getAllPokemon(initialUrl);
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      let pokemon = await loadingPokemon(response.results);
+      setLoading(false);
+    }
+    fetchData();
+  },[]);
 
-  if (pokemonData) {
-    return (
-        <div>{JSON.stringify(pokemonData)}</div>
-    );
+  const loadingPokemon = async (data) => {
+    let _pokemon = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon.url);
+      return pokemonRecord
+    }))
+
+    setPokemonData(_pokemon)
   }
+
+  console.log(pokemonData)
+  return(
+      <div>
+        {
+          loading ? <h1>Loading...</h1> : (
+              <>
+                <div className={"grid-container"}>
+                  {pokemonData.map((pokemon, i) => {
+                    return <Card key={i} pokemon={pokemon}/>
+                  })}
+                </div>
+              </>
+        )
+      }
+      </div>
+  );
+
+//   useEffect(() => {
+//     fetch(initialUrl)
+//         .then(res => res.json())
+//         .then(setPokemonData)
+//   },[initialUrl])
+//
+//
+//     if (pokemonData) {
+//       for (let i = 0; i < pokemonData.length; i++) {
+//         return (
+//             <div>
+//               <h1>{pokemonData.name}</h1>
+//             </div>
+//         );
+//       }
+//     }else {
+//       return <div>Sorry no pokemon</div>
+//     }
 }
+
 
 
 export default App;
